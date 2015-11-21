fa57df212c0ffc45ad426d4d2108526e74aa42e3
Ubaldino Zurita <ozzirisc@gmail.com>
2015-11-22 01:10:25 +0400

    [EDIT] creando la arquitectura control

--- control/control.py
+++ control/control.py
@@ -1,4 +1,4 @@
-#Fabrica de Software.
+ #Fabrica de Software.
 #Edgar E. Mamani Apaza.
 #UMSS-Ing. Electronica
 #refactor Jorge Encinas
@@ -8,83 +8,91 @@ import RPi.GPIO as GPIO
 from time import sleep
 
 
-GPIO.setmode(GPIO.BCM)
-GPIO.setwarnings(False)
+class Control:
+    """control para el DRONE"""
+    def __init__( self ):
+        GPIO.setmode( GPIO.BCM )
+        GPIO.setwarnings( False )
 
-freq=50
-dutyC=0
+        self.freq = 50
+        self.dutyC = 0
 
-valMinimo = 4.5
-valMaximo = 9.5
+        self.valMinimo = 4.5
+        self.valMaximo = 9.5
 
-m1=17
-m2=27
-m3=22
-m4=18
+        self.m1 = 17
+        self.m2 = 27
+        self.m3 = 22
+        self.m4 = 18
 
-relay=23
+        self.relay = 23
 
-tiempo = 2.3
+        self.tiempo = 2.3
 
-GPIO.setup(m1, GPIO.OUT)
-GPIO.setup(m2, GPIO.OUT)
-GPIO.setup(m3, GPIO.OUT)
-GPIO.setup(m4, GPIO.OUT)
+        GPIO.setup( self.m1 , GPIO.OUT )
+        GPIO.setup( self.m2 , GPIO.OUT )
+        GPIO.setup( self.m3 , GPIO.OUT )
+        GPIO.setup( self.m4 , GPIO.OUT )
 
-GPIO.setup(relay, GPIO.OUT)
-GPIO.output(relay, 0)
+        GPIO.setup( self.relay , GPIO.OUT )
+        GPIO.output( self.relay , 0 )
 
-motor01 = GPIO.PWM(m1, freq)
-motor02 = GPIO.PWM(m2, freq)
-motor03 = GPIO.PWM(m3, freq)
-motor04 = GPIO.PWM(m4, freq)
+        self.motor01 = GPIO.PWM( self.m1 , self.freq )
+        self.motor02 = GPIO.PWM( self.m2 , self.freq )
+        self.motor03 = GPIO.PWM( self.m3 , self.freq )
+        self.motor04 = GPIO.PWM( self.m4 , self.freq )
 
-motor01.start(0)
-motor02.start(0)
-motor03.start(0)
-motor04.start(0)
-
-print "inciciando en 2 seg"
-
-def setMotores(vel01, vel02, vel03, vel04):
-	motor01.ChangeDutyCycle(vel01)
-	motor02.ChangeDutyCycle(vel02)
-	motor03.ChangeDutyCycle(vel03)
-	motor04.ChangeDutyCycle(vel04)
+        self.motor01.start( 0 )
+        self.motor02.start( 0 )
+        self.motor03.start( 0 )
+        self.motor04.start( 0 )
 
 
-def arrancar():
-        sleep(2)
-        GPIO.output(relay, 1)
-        sleep(tiempo)
-	setMotores(valMaximo,valMaximo,valMaximo,valMaximo)
-	sleep(4)
-	print "configurando en bajo los motoresen 4 seg."
-	setMotores(valMinimo,valMinimo,valMinimo,valMinimo)
-	sleep(5)
-	print "configurando en bajo los motoresen 5 seg."
+    def setMotores( self, vel01 , vel02 , vel03 , vel04 ):
+        self.motor01.ChangeDutyCycle( vel01 )
+        self.motor02.ChangeDutyCycle( vel02 )
+        self.motor03.ChangeDutyCycle( vel03 )
+        self.motor04.ChangeDutyCycle( vel04 )
 
-def mapeo(valor):
-	if valor<=0:
-        	valor=0
-        elif valor>=100:
-                valor=100
-        return ((valor*0.05)+4.5)
 
-def interrumpir():
-	motor01.stop()
-        motor02.stop()
-        motor03.stop()
-        motor04.stop()
-        GPIO.output(relay, 0)
+    def arrancar( self ):
+        sleep( 2 )
+        GPIO.output( self.relay , 1 )
+        sleep( self.tiempo )
+        self.setMotores( valMaximo , valMaximo , valMaximo , valMaximo )
+        sleep( 4 )
+        print "configurando en bajo los motoresen 4 seg."
+        self.setMotores( valMinimo , valMinimo , valMinimo , valMinimo )
+        sleep( 5 )
+        print "configurando en bajo los motoresen 5 seg."
+
+    def mapeo( valor ):
+        if valor <= 0: valor = 0
+        elif valor >= 100: valor = 100
+        return ( ( valor * 0.05 ) + 4.5 )
+
+    def interrumpir():
+        self.motor01.stop()
+        self.motor02.stop()
+        self.motor03.stop()
+        self.motor04.stop()
+        GPIO.output( self.relay , 0 )
         GPIO.cleanup()
 
-try:
-	arrancar()
-	while True:
-		dutyC=float(raw_input("Ingresa la velocidad entre (1 - 100): "))
-		dutyC=mapeo(dutyC)
-		setMotores(dutyC, dutyC, dutyC, dutyC)
+    def test(self):
+        print "test"
 
+#control = Control()
+#control.test()
+"""
+
+try:
+    print "inciciando en 2 seg"
+    arrancar()
+    while True:
+        dutyC = float( raw_input( "Ingresa la velocidad entre (1 - 100): " ) )
+        dutyC = mapeo( dutyC )
+        setMotores( dutyC , dutyC , dutyC , dutyC )
 except KeyboardInterrupt:
-	interrumpir()
+    interrumpir()
+"""
