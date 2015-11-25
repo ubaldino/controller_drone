2e62b1c761cca0e7976b3e85df1a711510f5d06a
Ubaldino Zurita <ozzirisc@gmail.com>
2015-11-24 20:10:45 -0400

    [EDIT] prueba arduino

--- socket/server.py
+++ socket/server.py
@@ -1,26 +1,18 @@
-#!/usr/bin/env python
-#
-# Copyright 2009 Facebook
-#
-# Licensed under the Apache License, Version 2.0 (the "License"); you may
-# not use this file except in compliance with the License. You may obtain
-# a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
-# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
-# License for the specific language governing permissions and limitations
-# under the License.
 
-import os , sys
+import os , sys , serial
+
+serial_com = serial.Serial(
+    port = '/dev/ttyUSB0',
+    baudrate = 9600 ,
+    timeout = 1
+)
+
+serial_com.write( "s\n" )
+serial_com.write( "s\n" )
 
 DIR = os.path.dirname(__file__)
 
 sys.path.append(  os.path.join( DIR , '../control' )  )
-
-from control import Control
 
 import logging
 import tornado.escape
@@ -36,8 +28,6 @@ from tornado.options import define, options, parse_command_line
 define("port", default=7777, help="run on the given port", type=int)
 define("debug", default=False, help="run in debug mode")
 print "server in  port: 7777"
-
-CONTROL = Control()
 
 class MessageBuffer(object):
     def __init__(self):
@@ -87,63 +77,39 @@ class MainHandler(tornado.web.RequestHandler):
         self.render( "index.html" , messages=global_message_buffer.cache )
 
 
-class MessageNewHandler(tornado.web.RequestHandler):
-    def post(self):
-        message = {
-            "id": str(uuid.uuid4()),
-            "body": self.get_argument("body"),
-        }
-        # to_basestring is necessary for Python 3's json encoder,
-        # which doesn't accept byte strings.
-        message["html"] = tornado.escape.to_basestring(
-            self.render_string("message.html", message=message))
-        if self.get_argument("next", None):
-            self.redirect(self.get_argument("next"))
-        else:
-            self.write(message)
-        global_message_buffer.new_messages([message])
-
-
-class MessageUpdatesHandler(tornado.web.RequestHandler):
-    @gen.coroutine
-    def post(self):
-        cursor = self.get_argument("cursor", None)
-        # Save the future returned by wait_for_messages so we can cancel
-        # it in wait_for_messages
-        self.future = global_message_buffer.wait_for_messages(cursor=cursor)
-        messages = yield self.future
-        if self.request.connection.stream.closed():
-            return
-        self.write(dict(messages=messages))
-
-    def on_connection_close(self):
-        global_message_buffer.cancel_wait(self.future)
-
 class control_action_all( tornado.web.RequestHandler ):
     def post( self ):
         #pprint.pprint( self.request.arguments , width=1 )
         pprint.pprint( self.get_argument( "vel" ) )
 
-        vel = float( self.get_argument( "vel" ) )
+        vel = int( float( self.get_argument( "vel" ) ) )
 
-        CONTROL.setMotores( vel , vel , vel , vel )
+        serial_com.write( "v%d\n"%(vel) )
+
+class control_action_motor( tornado.web.RequestHandler ):
+    def post( self ):
+        #pprint.pprint( self.request.arguments , width=1 )
+        pprint.pprint( self.get_argument( "vel" ) )
+        num_motor = self.get_argument( "num_motor" )
+        vel = int( float( self.get_argument( "vel" ) ) )
+
+
+        serial_com.write( "v%d\n"%(vel) )
 
 class control_action_on_off( tornado.web.RequestHandler ):
     def post( self ):
         if self.get_argument("value") == 'on':
             print "encender"
-            CONTROL.iniciar()
+            serial_com.write( "c\n" )
         elif self.get_argument("value") == 'off':
             print "apagar"
-            CONTROL.interrumpir()
+            serial_com.write( "s\n" )
 
 def main():
     parse_command_line()
     app = tornado.web.Application(
         [
             (r"/", MainHandler),
-            (r"/a/message/new", MessageNewHandler),
-            (r"/a/message/updates", MessageUpdatesHandler),
             (r"/control/action/on_off" , control_action_on_off ),
             (r"/control/action/all" , control_action_all )
         ],
@@ -157,6 +123,10 @@ def main():
     tornado.ioloop.IOLoop.current().start()
     tornado.autoreload.wait()
 
-
 if __name__ == "__main__":
-    main()
+    try:
+        main()
+    except KeyboardInterrupt:
+        serial_com.write( "s\n" )
+        serial_com.close()
+        print "Sever Close and Serial <Close></Close>"
